d3d0c0d5eea200b4d7d8842a6e8f2ea4ed8d8d99
andrey.hlopkov <andrey.hlopkov@red-soft.ru>
2022-08-15 17:18:42 +0300

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+bob = Person("Bob Smith")
+sue = Person("Sue Jones", job='dev', pay=100000)
+
+if __name__=='__main__':
+    print(bob.name, bob.pay)
+    print(sue.name, sue.pay)
